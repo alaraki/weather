aeef689b40ca722c3b444d682c987478c3708e12
alaraki <alaraki2000@hotmail.com>
2023-11-03 16:52:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather .col .weathericon");
+const weatherIcon = document.querySelector(".weather .weathericon");
 const weatherState = document.querySelector(".state");
 
 async function checkWeather(city) {
